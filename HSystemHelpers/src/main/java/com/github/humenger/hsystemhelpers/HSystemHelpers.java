232031badb4beba55b3aca9199acab7c83e40b96
Humenger <louis4china@gmail.com>
2022-09-17 22:06:39 +0800

    bugfix

--- HSystemHelpers/src/main/java/com/github/humenger/hsystemhelpers/HSystemHelpers.java
+++ HSystemHelpers/src/main/java/com/github/humenger/hsystemhelpers/HSystemHelpers.java
@@ -2,24 +2,27 @@ package com.github.humenger.hsystemhelpers;
 
 import android.content.Context;
 
+import net.dongliu.apk.parser.ApkFile;
+import net.dongliu.apk.parser.bean.DexClass;
+
 import java.io.File;
 
 import dalvik.system.DexClassLoader;
 
 public class HSystemHelpers {
     public static final String TAG = "HSystemHelpers";
-    public static String findSystemJarPathWithClassName(Context context,String className){
-
+    public static String findSystemJarPathWithClassName(String className){
         String sysJarList[]=System.getenv("BOOTCLASSPATH").split(":");
-        String hshCache = context.getDir("HSH_cache", Context.MODE_PRIVATE).getPath();
-        DexClassLoader classLoader;
         for (String jarPath:sysJarList){
-            classLoader=new DexClassLoader(jarPath,hshCache,null,null);
-            try {
-                classLoader.loadClass(className);
-                return jarPath;
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
+            try{
+                ApkFile apkFile=new ApkFile(new File(jarPath));
+                for(DexClass cls:apkFile.getDexClasses()){
+                    if(cls.toString().equals("L"+className.replace('.','/')+";")){
+                        return jarPath;
+                    }
+                }
+            }catch (Exception ioException){
+                ioException.printStackTrace();
             }
         }
         return "";
