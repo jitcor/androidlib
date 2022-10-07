fe46805977eec5858345e66225a6e26c5bfcabda
humenger <louis4china@gmail.com>
2022-10-08 03:33:54 +0800

    opt(proxy):headers null 问题

--- GithubProxyHelpers/src/main/java/com/github/humenger/githubproxyhelpers/ProxyUrl.java
+++ GithubProxyHelpers/src/main/java/com/github/humenger/githubproxyhelpers/ProxyUrl.java
@@ -6,4 +6,7 @@ public class ProxyUrl {
     public String proxyUrl;
     public String originalUrl;
     public Map<String,String> headers;
+    public Map<String, String> getHeaders(Map<String,String> def) {
+        return headers==null?def:headers;
+    }
 }
