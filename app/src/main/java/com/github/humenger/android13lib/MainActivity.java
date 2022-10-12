bcf9ad3db0d6bd1277ac5c68d95005b54be0a80d
humenger <louis4china@gmail.com>
2022-10-12 09:36:03 +0800

    sync

--- app/src/main/java/com/github/humenger/android13lib/MainActivity.java
+++ app/src/main/java/com/github/humenger/android13lib/MainActivity.java
@@ -18,6 +18,26 @@ public class MainActivity extends PreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RSharedPreferences.getSharedPreferences(this,"www", Context.MODE_WORLD_READABLE);
+//        RSharedPreferences.getSharedPreferences(this,"www", Context.MODE_WORLD_READABLE);
+        FetchConfiguration fetchConfiguration = new FetchConfiguration.Builder(this)
+                .setDownloadConcurrentLimit(3)
+                .build();
+
+        fetch = Fetch.Impl.getInstance(fetchConfiguration);
+
+        String url = "http:www.example.com/test.txt";
+        String file = "/downloads/test.txt";
+
+        final Request request = new Request(url, file);
+        request.setPriority(Priority.HIGH);
+        request.setNetworkType(NetworkType.ALL);
+        request.addHeader("clientKey", "SD78DF93_3947&MVNGHE1WONG");
+
+        fetch.enqueue(request, updatedRequest -> {
+            //Request was successfully enqueued for download.
+        }, error -> {
+            //An error occurred enqueuing the request.
+        });
+
     }
 }
