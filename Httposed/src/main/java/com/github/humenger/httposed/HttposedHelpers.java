84939ac5c84a91052abcbd88a6974f8fd02587c9
humenger <louis4china@gmail.com>
2023-04-20 05:10:17 +0800

    阿文Sega是

---
+++ Httposed/src/main/java/com/github/humenger/httposed/HttposedHelpers.java
@@ -0,0 +1,12 @@
+package com.github.humenger.httposed;
+
+import java.util.HashMap;
+
+public class HttposedHelpers {
+    public static final String TAG = "HttposedHelpers";
+    private static final HashMap<String, String> uriCache = new HashMap<>();
+    public static HC_UriHook.Unhook hookUri(String host, String uri, HC_UriHook hook){
+
+        return null;
+    }
+}
