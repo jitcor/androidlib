2809b65918834583470cc7c225189c3b3262d946
humenger <louis4china@gmail.com>
2023-06-17 09:22:55 +0800

    而是态度就是让他的房间染头发

---
+++ app/src/main/java/com/github/humenger/android13lib/App.java
@@ -0,0 +1,16 @@
+package com.github.humenger.android13lib;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.github.humenger.githubproxyhelpers.GithubProxyHelpers;
+import com.github.humenger.hsystemhelpers.HSystemHelpers;
+
+public class App extends Application {
+    public static final String TAG="App";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
